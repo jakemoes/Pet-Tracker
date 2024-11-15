011cfc3d91219cc959ad9b3afc67f1857104c6db
jakemoes <jake.moe@icloud.com>
2024-11-15 14:37:22 +0100

    02.2 make add pet openable

--- src/app/src/main/java/com/example/pettracker/Wellcome.java
+++ src/app/src/main/java/com/example/pettracker/Wellcome.java
@@ -2,6 +2,7 @@
 
 package com.example.pettracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +30,8 @@ public class Wellcome extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Show a toast message when the button is clicked
-                
+                Intent intent = new Intent(Wellcome.this, AddAnimal.class);
+                startActivity(intent);
             }
         });
     }
